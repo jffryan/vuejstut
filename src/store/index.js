7c02adb3d33bf61f3ae1f1b5bbe928612d335b0e
jffryan <jffryan@gmail.com>
2022-08-27 13:10:09 -0500

    basic state and mutations set up

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,28 @@
 import { createStore } from "vuex";
 
-export default createStore({
-  state: {},
+export const state = () => {
+  return {
+    isLoggedIn: false,
+  };
+};
+
+export const mutations = {
+  // Mutations need to be synchronous methods
+  LOGIN_USER(state) {
+    state.isLoggedIn = true;
+  },
+  LOGOUT_USER(state) {
+    state.isLoggedIn = false;
+  },
+};
+
+const store = createStore({
+  state,
+  mutations,
   getters: {},
-  mutations: {},
   actions: {},
   modules: {},
+  strict: process.env.NODE_ENV !== "production",
 });
+
+export default store;
