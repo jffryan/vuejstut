55f6fc866bd954214ff3774bb7c26bdf76e045a2
jffryan <jffryan@gmail.com>
2022-08-28 14:36:41 -0500

    adds testing to action module

--- tests/unit/store/index.test.js
+++ tests/unit/store/index.test.js
@@ -1,4 +1,7 @@
-import { state, mutations } from "@/store";
+import { state, mutations, actions } from "@/store";
+
+import getJobs from "@/api/getJobs";
+jest.mock("@/api/getJobs");
 
 describe("state", () => {
   it("keeps track of whether user is logged in", () => {
@@ -31,6 +34,35 @@ describe("mutations", () => {
       const state = { jobs: [] };
       mutations.RECEIVE_JOBS(state, ["Job1", "Job2"]);
       expect(state).toEqual({ jobs: ["Job1", "Job2"] });
+    });
+  });
+});
+
+describe("actions", () => {
+  describe("FETCH_JOBS", () => {
+    beforeEach(() => {
+      getJobs.mockResolvedValue([
+        {
+          id: 1,
+          title: "Software Developer",
+        },
+      ]);
+    });
+    it("makes a request to fetch jobs", async () => {
+      const context = { commit: jest.fn() };
+      await actions.FETCH_JOBS(context);
+      expect(getJobs).toHaveBeenCalled();
+    });
+    it("sends message to save received jobs in store", async () => {
+      const commit = jest.fn();
+      const context = { commit };
+      await actions.FETCH_JOBS(context);
+      expect(commit).toHaveBeenCalledWith("RECEIVE_JOBS", [
+        {
+          id: 1,
+          title: "Software Developer",
+        },
+      ]);
     });
   });
 });
